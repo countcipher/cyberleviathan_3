2fa189d6f13723f277c80c0349152c2b2601ee40
Count Cipher <chrisharlow366@yahoo.com>
2020-08-25 01:58:29 -0400

    com

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
-let nav = document.querySelector(".nav");
-let navLinkTitle = document.querySelectorAll(".nav__link-title");
+const nav = document.querySelector(".nav"),
+   navLinkTitle = document.querySelectorAll(".nav__link-title"),
+   navDropdownBox = document.querySelectorAll(".nav__dropdown-box");
+   navDropdownContent = document.querySelectorAll(".nav__dropdown-content");
 
-window.addEventListener("scroll", myFunc);
 
-function myFunc(){
+window.addEventListener("scroll", navWhiteOnScroll);
+navLinkTitle[1].addEventListener("click", showDropdownContent);
+
+function navWhiteOnScroll(){
    if(window.pageYOffset >= 300 && window.innerWidth >= 692){
         nav.classList.add("onScrollWhite");
         navLinkTitle.forEach(function(item){
@@ -15,4 +19,10 @@ function myFunc(){
          item.classList.remove("onScrollBlackColor");
       })
    }
+}
+
+function showDropdownContent(){
+   navDropdownContent[0].classList.toggle("fullHeight");
+   //navDropdownContent[0].style.height = "100%";
+   console.log('hit');
 }
