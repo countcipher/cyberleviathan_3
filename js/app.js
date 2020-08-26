db2645954a7115e86cd594546482de40608d9866
Count Cipher <chrisharlow366@yahoo.com>
2020-08-26 01:28:10 -0400

    com

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+//============================
+//DEFINED CONSTANTS VARIABLES
+//============================
 const body = document.querySelector("body"),
    html = document.querySelector("html"),
    fullWrapper = document.querySelector("full-wrapper");
@@ -8,17 +11,25 @@ const body = document.querySelector("body"),
    navDropdownContent = document.querySelectorAll(".nav__dropdown-content"),
    hamburger = document.querySelector(".hamburger");
 
-
+//=============================
+//EVENT LISTENERS
+//=============================
 window.addEventListener("scroll", navWhiteOnScroll);
 window.addEventListener("resize", removeMobileClasses);
 hamburger.addEventListener("click", toggleMenu);
 
+//==============================
+//GIVES <li/> WITH "HAS-DROPDOWN" CLASS THE showDropdownContent FUNCTION
+//==============================
 (function(){
    hasDropdown.forEach(function(item){
       item.addEventListener("click", showDropdownContent);
    })
 })();
 
+//=================================
+//SETS CSS PROPERTIES FOR NAV AFTER SCROLLING DOWN PAGE
+//=================================
 function navWhiteOnScroll(){
    if(window.pageYOffset >= 300 && window.innerWidth >= 646){
         nav.classList.add("onScrollWhite");
@@ -33,20 +44,33 @@ function navWhiteOnScroll(){
    }
 }
 
+//================================
+//SHOWS DROPDOWN CONTENT FOR DROPDOWN MENUS
+//================================
 function showDropdownContent(){
    this.children[1].classList.toggle("fullHeight");
 }
 
+//==================================
+//REMOVES ALL MOBILE-USE CLASSES WHEN WINDOW IS RESIZED
+//==================================
 function removeMobileClasses(){
    if(window.innerWidth >= 646){
       navDropdownContent.forEach(function(item){
          item.classList.remove("fullHeight");
       })
       navLinks.classList.remove("menuShow");
+      nav.classList.remove("fullVH");
+      body.classList.remove("noScroll");
+      html.classList.remove("noScroll");
+      fullWrapper.classList.remove("noScroll");
    }
    
 }
 
+//====================================
+//OPENS AND CLOSES THE MOBILE NAV MENU
+//====================================
 function toggleMenu(){
    nav.classList.toggle("fullVH");
    navLinks.classList.toggle("menuShow");
