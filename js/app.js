36a54586453c2f0f4d144438b0594178906626a3
Count Cipher <chrisharlow366@yahoo.com>
2020-08-27 00:46:00 -0400

    com

--- js/app.js
+++ js/app.js
@@ -17,6 +17,8 @@ const body = document.querySelector("body"),
 //=============================
 window.addEventListener("scroll", navWhiteOnScroll);
 window.addEventListener("resize", removeMobileClasses);
+window.addEventListener("resize", removeLargeScreenClasses);
+window.addEventListener("resize", addLargeScreenClasses);
 hamburger.addEventListener("click", toggleMenu);
 
 //==============================
@@ -45,6 +47,25 @@ function navWhiteOnScroll(){
    }
 }
 
+//====================================
+//ADDS NECESSARY CLASSES TO NAVIGATION
+//IN THE EVENT THAT SOMEONE, LIKE ME,
+//WERE TO OPEN THE SITE IN MOBILE VERSION,
+//SCROLL DOWN A WAYS,
+//AND THEN DECIDE TO ENLARGE THE WINDOW.
+//THIS CODE WILL MAKE IT SO THAT THE NAVIGATION
+//IS WHITE BACKGROUND WITH BLACK LETTERING
+//IF THE PAGE SCOLLED DOWN BEYOND 144 (>= 145)
+//=========================================
+function addLargeScreenClasses(){
+   if(window.innerWidth >= 646 && window.pageYOffset >=145){
+      nav.classList.add("onScrollWhite");
+      navLinkTitle.forEach(function(title){
+         title.classList.add("onScrollBlackColor");
+      });
+   }
+}
+
 //================================
 //SHOWS DROPDOWN CONTENT FOR DROPDOWN MENUS
 //================================
@@ -53,7 +74,7 @@ function showDropdownContent(){
 }
 
 //==================================
-//REMOVES ALL MOBILE-USE CLASSES WHEN WINDOW IS RESIZED
+//REMOVES ALL MOBILE-USE CLASSES WHEN WINDOW IS RESIZED FROM MOBILE TO LARGE
 //==================================
 function removeMobileClasses(){
    if(window.innerWidth >= 646){
@@ -65,13 +86,34 @@ function removeMobileClasses(){
       body.classList.remove("noScroll");
       html.classList.remove("noScroll");
       fullWrapper.classList.remove("noScroll");
-   }
+//=====================================
+//THIS IS THE BLOCK OF CODE
+//THAT'S NEED TO BE REDONE TO MAKE THIS SHIT WORK
+//SCOLLING DOWN AND THEN SLIGHTLY BACK UP
+//ON PC SIZE SCREEN, THEN GOING TO MOBILE SIZE
+//MAKES THE COLOR OF THE NAV BLACK
+//FIX THIS SHIT!!!
+//======================================
+   // }else if(window.innerWidth < 646){
+   //    nav.classList.remove("onScrollWhite");
+   //    navLinkTitle.classList.add("whiteColor");
+   //    navLinkTitle.classList.remove("onScrollBlackColor");
+   //    alert('hello');
+    }
+   
+}
+
+//===================================
+//REMOVES CLASSES SPECIFIC TO LARGE SIZE
+//TO PREP FOR MOBILE LAYOUT
+//====================================
+function removeLargeScreenClasses(){
    if(window.innerWidth < 646){
       nav.classList.remove("onScrollWhite");
-      //navLinkTitle.classList.add("whiteColor");
-      navLinkTitle.classList.remove("onScrollBlackColor");
+      navLinkTitle.forEach(function(item){
+         item.classList.remove("onScrollBlackColor");
+      });
    }
-   
 }
 
 //====================================
