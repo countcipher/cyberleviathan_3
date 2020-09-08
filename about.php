63bae88464fe139c16db4c7805b15627de7f2c15
Count Cipher <chrisharlow366@yahoo.com>
2020-09-08 13:08:56 -0400

    com

--- about.php
+++ about.php
@@ -8,14 +8,34 @@ include "includes/header.php";
 
 <?php include "includes/navigation.php"; ?>
 
-<!-- ===============================
-              HEADER
-================================= -->
-<header class="header--small">
-    <h1>About</h1>
-</header>
+    <!-- ===============================
+                HEADER
+    ================================= -->
+    <header class="header_small header_small--aboutPage">
+        <div class="header_small__text-box">
+                    <h1>About</h1>
+                    <h2>we empower your business</h2>
+        </div>
+    </header>
 
- <!-- ==============================
-                        FOOTER
-================================ -->
-<?php include "includes/footer.php" ?>
+    <!-- =============================
+                MAIN HEADING
+    =============================== -->
+    <section class="about-about">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At a inventore vel, eos totam odit debitis, harum mollitia atque amet sapiente ea quisquam illum in, nam maiores quis maxime ad. Rerum quod ducimus veniam tempora?</p>
+    </section>
+
+    <!-- ==============================
+                VIDEO
+    ================================ -->
+    <section class="about-video">
+        <video class="about-video__video" controls>
+            <source src="video/website_vid.mp4" type="video/mp4">
+        </video>
+    </section>
+
+    <!-- ==============================
+                FOOTER
+    ================================ -->
+    <?php include "includes/footer.php" ?>
