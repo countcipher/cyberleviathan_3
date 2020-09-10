5510493bd1c4a297e4246c89087bdcf7d3d29827
Count Cipher <chrisharlow366@yahoo.com>
2020-09-10 14:16:53 -0400

    com

--- index.php
+++ index.php
@@ -135,13 +135,7 @@ include "includes/header.php";
         <!-- =============================
                         CONTACT
         =============================== -->
-        <section class="contact">
-                <div class="box-1">
-                    <h2>Cyber Leviathan gives you everything your business needs to stand out and be effective online. So what are you waiting for?</h2>
-
-                    <a href="#"><button class="btn btn--cta">Get Started</button></a>
-                </div>
-        </section>
+        <?php include "includes/contact.php"; ?>
         
         <!-- ==============================
                         FOOTER
