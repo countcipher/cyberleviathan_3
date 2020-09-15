63114bf7d20445dfdd624a650cc7fb7a604bd59d
Count Cipher <chrisharlow366@yahoo.com>
2020-09-15 11:38:22 -0400

    com

--- custom-websites.php
+++ custom-websites.php
@@ -19,6 +19,14 @@ include "includes/header.php";
     </header>
 
     <!-- =============================
+                MAIN HEADING
+    =============================== -->
+    <section class="custom-websites__about">
+        <h1>Go from "I <em>need</em> a great website" to "I <em>have</em> a great website"</h1>
+        <p>Based in Staunton, VA, Cyber Leviathan offers professional online services to businesses of all sizes.  Whether you're just beginning your small business or are a part of a large company that has been operating for decades, we have the skills and experience to empower your business online!</p>
+    </section>
+
+    <!-- =============================
                         CONTACT
     =============================== -->
     <?php include "includes/contact.php"; ?>
