ef0cff89c72875ba637a5982e5ff68a32c9f6fba
Count Cipher <chrisharlow366@yahoo.com>
2020-09-18 11:55:57 -0400

    com

---
+++ digital-marketing.php
@@ -0,0 +1,114 @@
+<?php
+
+$title = "Custom Websites";
+
+include "includes/header.php";
+
+?>
+
+<?php include "includes/navigation.php"; ?>
+
+    <!-- ===============================
+                HEADER
+    ================================= -->
+    <header class="header_small header_small--custom-websitesPage">
+        <div class="header_small__text-box">
+                    <h1>Custom Websites</h1>
+                    <h2>Specifically for your business</h2>
+        </div>
+    </header>
+
+    <!-- =============================
+                MAIN HEADING
+    =============================== -->
+    <section class="custom-websites__about">
+        <h1>Go from "I <em>need</em> a great website" to "I <em>have</em> a great website"</h1>
+        <div class="box-1">
+            <div class="box-2">
+                <img src="images/woman_with_computer2.jpg" alt="">
+            </div>
+
+            <div class="box-2">
+                <p>All Cyber Leviathan websites are custom-built for you. We find out about your needs, your target audience and even your competition in order to craft the perfect site for you.</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- ===========================
+                FEATURED SITES
+    ============================ -->
+    <section class="custom-websites__featured-sites">
+        <h1>Featured sites</h1>
+        <div class="box-1">
+            <div class="box-2">
+                <div class="custom-websites__featured-sites__site custom-websites__featured-sites__site--dd">
+                    <div class="custom-websites__featured-sites__link">
+                        <a href="#"><button class="btn btn--cta">Visit Site</button></a>
+                    </div>
+                
+                </div>
+                <a href="#"><button class="btn btn--cta btn--mobile">Visit Site</button></a>
+            </div>
+
+            <div class="box-2">
+                <div class="custom-websites__featured-sites__site custom-websites__featured-sites__site--wwt">
+                    <div class="custom-websites__featured-sites__link">
+                            <a href="#"><button class="btn btn--cta">Visit Site</button></a>
+                        </div>
+                    </div>
+                <a href="#"><button class="btn btn--cta btn--mobile">Visit Site</button></a>
+            </div>
+        </div>
+
+        <div class="box-1">
+            <div class="box-2">
+                <div class="custom-websites__featured-sites__site custom-websites__featured-sites__site--ts">
+                    <div class="custom-websites__featured-sites__link">
+                        <a href="#"><button class="btn btn--cta">Visit Site</button></a>
+                    </div>
+                
+                </div>
+                <a href="#"><button class="btn btn--cta btn--mobile">Visit Site</button></a>
+            </div>
+
+            <div class="box-2">
+                <div class="custom-websites__featured-sites__site custom-websites__featured-sites__site--gym">
+                    <div class="custom-websites__featured-sites__link">
+                            <a href="#"><button class="btn btn--cta">Visit Site</button></a>
+                        </div>
+                    </div>
+                <a href="#"><button class="btn btn--cta btn--mobile">Visit Site</button></a>
+            </div>
+        </div>
+
+        <div class="box-1">
+            <div class="box-2">
+                <div class="custom-websites__featured-sites__site custom-websites__featured-sites__site--ec">
+                    <div class="custom-websites__featured-sites__link">
+                        <a href="#"><button class="btn btn--cta">Visit Site</button></a>
+                    </div>
+                
+                </div>
+                <a href="#"><button class="btn btn--cta btn--mobile">Visit Site</button></a>
+            </div>
+
+            <div class="box-2">
+                <div class="custom-websites__featured-sites__site custom-websites__featured-sites__site--bj">
+                    <div class="custom-websites__featured-sites__link">
+                            <a href="#"><button class="btn btn--cta">Visit Site</button></a>
+                        </div>
+                    </div>
+                <a href="#"><button class="btn btn--cta btn--mobile">Visit Site</button></a>
+            </div>
+        </div>
+    </section>
+
+    <!-- =============================
+                        CONTACT
+    =============================== -->
+    <?php include "includes/contact.php"; ?>
+
+    <!-- ==============================
+                FOOTER
+    ================================ -->
+    <?php include "includes/footer.php" ?>
