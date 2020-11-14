7f136e57593a7a19977c688a0a86ecc6392b34e2
Count Cipher <chrisharlow366@yahoo.com>
2020-11-14 11:58:13 -0500

    com

--- email.php
+++ email.php
@@ -21,7 +21,7 @@ include "includes/header.php";
     <!-- =============================
                 MAIN HEADING
     =============================== -->
-    <section class="custom-websites__about">
+    <section class="EMAIL__about">
         <h1>Customized email addresses prove your professionalism</h1>
         <div class="box-1">
             <div class="box-2">
@@ -40,51 +40,32 @@ include "includes/header.php";
     <!-- ===========================
                 FEATURED SITES
     ============================ -->
-    <section class="EMAIL_">
-    </section>
+    <!-- <section class="EMAIL_">
+    </section> -->
 
     <!-- =========================
             FEATURES
     ========================== -->
-    <section class="custom-websites__features">
+    <section class="EMAIL__features">
         <h1>key features</h1>
 
         <div class="box-1">
             <div class="box-3">
-                <i class="fas fa-building"></i>
-                <h2>Business specific</h2>
-                <p>No generic templates! Your site is customized to your specific business!</p>
+                <i class="fas fa-database"></i>
+                <h2>The Storage you need</h2>
+                <p>Get 10 GB to 50 GB of storage so that you have exactly what you need.</p>
             </div>
 
             <div class="box-3">
-                <i class="fas fa-search"></i>
-                <h2>SEO</h2>
-                <p>Search Engine Optimization (SEO) is the process of making your site easy to find with search engines like Google and Bing. Let Cyber Leviathan expertly optimize your site!</p>
+                <i class="fas fa-at"></i>
+                <h2>Domain based email</h2>
+                <p>People are 60% more likely to open an email that has the business name in the address.  Step away from Gmail and Yahoo! email accounts and step into professional email.</p>
             </div>
 
-            <div class="box-3">
-                <i class="fas fa-globe"></i>
-                <h2>client control</h2>
-                <p>Need to make regular updates to your site? You can take control of nearly any aspect of your site with Evolution. All you need is an Internet connection!</p>
-            </div>
-        </div>
-        <div class="box-1">
             <div class="box-3">
                 <i class="fas fa-mobile-alt"></i>
-                <h2>responsive design</h2>
-                <p>Your site is designed to look amazing and function properly on any device.</p>
-            </div>
-
-            <div class="box-3">
-                <i class="fas fa-chart-line"></i>
-                <h2>scalable</h2>
-                <p>Unsure if your site will need to have added features or content in the future? No worries! Your site will be designed with the potential for growth in mind.</p>
-            </div>
-
-            <div class="box-3">
-                <i class="fas fa-bug"></i>
-                <h2>Functionality Tested</h2>
-                <p>All Cyber Leviathan sites are meticulously tested to ensure there are no programming bugs and that they function as desired, no matter what.</p>
+                <h2>client control</h2>
+                <p>Need to make regular updates to your site? You can take control of nearly any aspect of your site with Evolution. All you need is an Internet connection!</p>
             </div>
         </div>
     </section>
