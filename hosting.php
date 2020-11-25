5c3829c3fcccb831113060afd1640cefed601fea
Count Cipher <chrisharlow366@yahoo.com>
2020-11-25 12:42:10 -0500

    com

--- hosting.php
+++ hosting.php
@@ -37,6 +37,59 @@ include "includes/header.php";
         </div>
     </section>
 
+    <!--================================= 
+                PACKAGES
+     ==================================-->
+     <!-- <section class="HOSTING__packages">
+         <h1>hello</h1>
+     </section> -->
+
+     <!-- =========================
+            FEATURES
+    ========================== -->
+    <section class="HOSTING__features">
+        <h1>key features</h1>
+
+        <div class="box-1">
+            <div class="box-3">
+                <i class="fas fa-building"></i>
+                <h2>Business specific</h2>
+                <p>No generic templates! Your site is customized to your specific business!</p>
+            </div>
+
+            <div class="box-3">
+                <i class="fas fa-search"></i>
+                <h2>SEO</h2>
+                <p>Search Engine Optimization (SEO) is the process of making your site easy to find with search engines like Google and Bing. Let Cyber Leviathan expertly optimize your site!</p>
+            </div>
+
+            <div class="box-3">
+                <i class="fas fa-globe"></i>
+                <h2>client control</h2>
+                <p>Need to make regular updates to your site? You can take control of nearly any aspect of your site with Evolution. All you need is an Internet connection!</p>
+            </div>
+        </div>
+        <div class="box-1">
+            <div class="box-3">
+                <i class="fas fa-mobile-alt"></i>
+                <h2>responsive design</h2>
+                <p>Your site is designed to look amazing and function properly on any device.</p>
+            </div>
+
+            <div class="box-3">
+                <i class="fas fa-chart-line"></i>
+                <h2>scalable</h2>
+                <p>Unsure if your site will need to have added features or content in the future? No worries! Your site will be designed with the potential for growth in mind.</p>
+            </div>
+
+            <div class="box-3">
+                <i class="fas fa-bug"></i>
+                <h2>Functionality Tested</h2>
+                <p>All Cyber Leviathan sites are meticulously tested to ensure there are no programming bugs and that they function as desired, no matter what.</p>
+            </div>
+        </div>
+    </section>
+
 
     <!-- =============================
                 CONTACT
