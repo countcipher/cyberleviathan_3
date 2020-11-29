32ed3c96b5ea8a9929b330715894fbef5df41a70
Count Cipher <chrisharlow366@yahoo.com>
2020-11-28 23:30:25 -0500

    com

---
+++ cl-advantage.php
@@ -0,0 +1,49 @@
+<?php
+
+$title = "CL Advantage";
+
+include "includes/header.php";
+
+?>
+
+<?php include "includes/navigation.php"; ?>
+
+    <!-- ===============================
+                HEADER
+    ================================= -->
+    <header class="header_small header_small--clAdvantagePage">
+        <div class="header_small__text-box">
+                    <h1>CL Advantage</h1>
+                    <h2>take your business to the next level</h2>
+        </div>
+    </header>
+
+    <!-- =============================
+                MAIN HEADING
+    =============================== -->
+    <section class="HOSTING__about">
+        <h1>Give your business the ultimate edge with CL Advantage</h1>
+        <div class="box-1">
+            <div class="box-2">
+                <img src="images/cl_advantage0.jpg" alt="">
+            </div>
+
+            <div class="box-2">
+                <p>Feel secure that your site is up and running the way that you need it to be for your business.  With uptime monitoring and industry-leading load time for your pages, you can rely on us.</p>
+
+                <span class="pricing">Prices starting at $3.99/mo</span>
+            </div>
+
+        </div>
+    </section>
+
+
+    <!-- =============================
+                CONTACT
+    =============================== -->
+    <?php include "includes/contact.php"; ?>
+
+    <!-- ==============================
+                FOOTER
+    ================================ -->
+    <?php include "includes/footer.php" ?>
