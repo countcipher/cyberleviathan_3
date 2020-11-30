f25d9e7c4cd2e532b25b014f83f7d5a3ab84cd4d
Count Cipher <chrisharlow366@yahoo.com>
2020-11-30 00:45:39 -0500

    com

--- cl-advantage.php
+++ cl-advantage.php
@@ -21,7 +21,7 @@ include "includes/header.php";
     <!-- =============================
                 MAIN HEADING
     =============================== -->
-    <section class="HOSTING__about">
+    <section class="CL-ADVANTAGE__about">
         <h1>Give your business the ultimate edge with CL Advantage</h1>
         <div class="box-1">
             <div class="box-2">
@@ -29,12 +29,37 @@ include "includes/header.php";
             </div>
 
             <div class="box-2">
-                <p>Feel secure that your site is up and running the way that you need it to be for your business.  With uptime monitoring and industry-leading load time for your pages, you can rely on us.</p>
+                <p>CL Advantage are the line of applications created directly by Cyber Leviathan.  With CL Advantage applications, you'll never have to worry about third-party applications breaking on your site. </p>
             </div>
-
         </div>
     </section>
 
+    <!-- =============================
+                AVAILABLE APS
+    =============================== -->
+    <section class="CL-ADVANTAGE__aps">
+        <div class="box-1">
+            <div class="box-2">
+                <div class="card">
+                    <img src="images/trident1.jpg" alt="">
+                    <!-- <h1>Evolution</h1> -->
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aspernatur earum repellendus iure! Quia consectetur vero excepturi. Perspiciatis voluptatibus, impedit nesciunt at deleniti qui dolores.</p>
+
+                    <a href="#"><button class="btn btn--cta">Learn More <i class="fas fa-arrow-right"></i></button></a>
+                </div>
+            </div>
+
+            <div class="box-2">
+                <div class="card">
+                    <img src="images/evolution0.jpg" alt="">
+                    <!-- <h1>Evolution</h1> -->
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aspernatur earum repellendus iure! Quia consectetur vero excepturi. Perspiciatis voluptatibus, impedit nesciunt at deleniti qui dolores.</p>
+
+                    <a href="#"><button class="btn btn--cta">Learn More <i class="fas fa-arrow-right"></i></button></a>
+                </div>
+            </div>
+        </div>
+    </section>
 
     <!-- =============================
                 CONTACT
