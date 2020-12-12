155400722b7b737f0f826966dfe2eb694b80429a
Count Cipher <chrisharlow366@yahoo.com>
2020-12-12 12:20:00 -0500

    com

--- evolution.php
+++ evolution.php
@@ -1,6 +1,6 @@
 <?php
 
-$title = "Trident";
+$title = "Evolution";
 
 include "includes/header.php";
 
@@ -11,29 +11,85 @@ include "includes/header.php";
     <!-- ===============================
                 HEADER
     ================================= -->
-    <header class="header_small header_small--tridentPage">
+    <header class="header_small header_small--evolutionPage">
         <div class="header_small__text-box">
-                    <h1>Trident</h1>
-                    <h2>CL Advantage contact form system</h2>
+                    <h1>Evolution</h1>
+                    <h2>CL Advantage content management system</h2>
         </div>
     </header>
 
     <!-- =============================
                 MAIN HEADING
     =============================== -->
-    <section class="TRIDENT__about">
-        <h1>connecting with your clients has never been so simple</h1>
+    <section class="EVOLUTION__about">
+        <h1>take control of your website</h1>
         <div class="box-1">
             <div class="box-2">
-                <img src="images/contact-form.jpg" alt="">
+                <img src="images/evolution0.jpg" alt="">
             </div>
 
             <div class="box-2">
-                <p>Trident is designed for businesses which have a brick-and-mortar location, or who sell services.  With Trident, you get a reliable connection between you and your customers.</p>
+                <p>Evolution is the content management system created by Cyber Leviathan.  With it, you can update images, text, video, and audio files to your website.  Whether you're on a desktop computer, laptop, tablet or smartphone, Evolution is the simple-to-use solution for businesses who need to make frequent updates to their website.</p>
 
-                <span class="pricing">Services at $9.99/mo</span>
+                <span class="pricing">prices starting at $89/mo</span>
             </div>
 
+        </div>
+    </section>
+
+    <!-- =============================
+                HOW IT WORKS
+    =============================== -->
+    <section class="EVOLUTION__how-it-works">
+        <!-- <h1>how it works</h1> -->
+
+        <div class="box-1">
+            <div class="box-2 img-box">
+                <img src="images/gym0.jpg" alt="">
+            </div>
+
+            <div class="box-2 text-box">
+                <h1>determine your needs</h1>
+
+                <p>Evolution can be set to allow control of any number of aspects of your website.  Choose which parts of it you want to be able to control, and we'll plug in Evolution to allow you to do just that.  And since Evolution is designed for total flexibility, you can increase or decrease the amount of content control you have at any time. </p>
+            </div>
+        </div>
+
+        <div class="box-1">
+            <div class="box-2 text-box">
+                <h1>get connected</h1>
+
+                <p>Visitors to your website use the contact form connected to Trident just like any other contact form.  The users interact with the form exactly as intended, with absolutely no intrusiveness from Trident.  Once the user submits the form, the information gets immediately sent to a database.</p>
+            </div>
+
+            <div class="box-2 img-box">
+                <img src="images/man-using-contact-form0.jpg" alt="">
+            </div>
+        </div>
+
+        <div class="box-1">
+            <div class="box-2 img-box">
+                <img src="images/trident/trident_login.jpg" alt="">
+            </div>
+
+            <div class="box-2 text-box">
+                <h1>log in to admin area</h1>
+
+                <p>Those administrators who have been given access to your Trident account log in with a username and password.  There, administrators have access to all of the features that Trident provides.</p>
+            </div>
+        </div>
+
+        <div class="box-1">
+
+            <div class="box-2 text-box">
+                <h1>a simple interface</h1>
+
+                <p>Trident displays all of the information in a way that is simple to use.  Sort the records by name and date, quickly delete old records, and list records as spam to help prevent them from coming back.  Add or remove administrators from your Trident account, and even add to your "Forbidden Words" list to help prevent unsolicited messages.</p>
+            </div>
+
+            <div class="box-2 img-box">
+                    <img src="images/trident/trident2.jpg" alt="">
+            </div>
         </div>
     </section>
 
