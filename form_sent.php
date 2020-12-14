ffabb114882feb5f70ada45403c8c13be4f9f0b0
Count Cipher <chrisharlow366@yahoo.com>
2020-12-13 23:44:47 -0500

    com

---
+++ form_sent.php
@@ -0,0 +1,22 @@
+<?php
+
+$title = "Thank You";
+
+include "includes/header.php";
+
+?>
+
+    <!-- ===============================
+                HEADER
+    ================================= -->
+    <section class="FORM-SENT">
+        <a href="index.php"><img src="images/logo_white.png" alt=""></a>
+        <h1>Thank you for contacting Cyber Leviathan!</h1>
+
+        <h3>Your information has been delivered.</h3>
+    </section>
+
+    <!-- ==============================
+                FOOTER
+    ================================ -->
+    <?php include "includes/footer.php" ?>
