3ee4edbdf4f847cda07a8e131027b39a6bd3ef5d
Count Cipher <chrisharlow366@yahoo.com>
2020-12-21 00:07:22 -0500

    com

---
+++ blog-template.php
@@ -0,0 +1,29 @@
+<?php
+
+$title = "Blog";
+
+include "includes/header.php";
+
+?>
+
+        <?php include "includes/navigation.php"; ?>
+        
+        <!-- ==================================
+                    HEADER
+        =====================================-->
+        <header class="header">
+            <div class="header__text-box">
+                <h1>Blog</h1>
+                <h2>Your resource center for business technology</h2>
+            </div>
+        </header>
+
+        <!-- =============================
+                        CONTACT
+        =============================== -->
+        <?php include "includes/contact.php"; ?>
+        
+        <!-- ==============================
+                        FOOTER
+        ================================ -->
+        <?php include "includes/footer.php" ?>
