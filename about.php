7a73a0d413ce824250f52d7be37a5e84f337cc46
Count Cipher <chrisharlow366@yahoo.com>
2021-03-02 11:27:10 -0500

    com

--- about.php
+++ about.php
@@ -21,34 +21,12 @@ include "includes/header.php";
     <!-- =============================
                 MAIN HEADING
     =============================== -->
-    <section class="about-about">
+    <!-- <section class="about-about">
         <h1>Based In Staunton, VA</h1>
         <p>Based in Staunton, VA, Cyber Leviathan offers professional online services to businesses of all sizes.  Whether you're just beginning your small business or are a part of a large company that has been operating for decades, we have the skills and experience to empower your business online!</p>
-    </section>
-
-    <!-- ==============================
-                VIDEO
-    ================================ -->
-    <!-- <section class="about-video">
-        <video class="about-video__video" controls>
-            <source src="video/website_vid.mp4" type="video/mp4">
-        </video>
     </section> -->
 
-    <!-- =============================
-                    TESTIMONIAL
-    =============================== -->
-    <section class="about-testimonial">
-        <div class="about-testimonial__testimonial">
-            <h1>Our Clients Say...</h1>
-            <p><i class="fas fa-quote-left"></i> Chris Harlow's knowledge and expertise is beyond remarkable. I am a wedding officiant and he created a stunning wedding website for me that is not only strategic and functional, but beautiful and has had so many compliments from my clients.
-            <br>
-            —Terry Eller, Weddings With Terry
-            </p>
-        </div>
-    </section>
-
-    <!-- =============================
+     <!-- =============================
                 WHY CYBER LEVIATHAN
     ============================== -->
     <section class="about-why">
@@ -77,26 +55,56 @@ include "includes/header.php";
         </div>
     </section>
 
+    <!-- ==============================
+                VIDEO
+    ================================ -->
+    <!-- <section class="about-video">
+        <video class="about-video__video" controls>
+            <source src="video/website_vid.mp4" type="video/mp4">
+        </video>
+    </section> -->
+
+    <!-- =============================
+                    TESTIMONIAL
+    =============================== -->
+    <section class="about-testimonial">
+        <div class="about-testimonial__testimonial">
+            <h1>Our Clients Say...</h1>
+            <p><i class="fas fa-quote-left"></i> Chris Harlow's knowledge and expertise is beyond remarkable. I am a wedding officiant and he created a stunning wedding website for me that is not only strategic and functional, but beautiful and has had so many compliments from my clients.
+            <br>
+            —Terry Eller, Weddings With Terry
+            </p>
+        </div>
+    </section>
+
+   
+
     <!--==============================
                 THE TEAM
      ==============================-->
      <section class="about-team">
-        <h1>we empower you</h1>
+        <h1>we empower your business</h1>
 
         <div class="box-1">
             <div class="box-3">
                 <img class="mug" src="images/team/chris_mug.jpg" alt="Chris Harlow photo">
                 <p class="name">Chris Harlow</p>
                 <p class="title">Web Design &amp; Development</p>
-                <div class="bio-button">Bio</div>
+                <div class="bio-button"><span>Bio</span></div>
             </div>
 
             <div class="box-3">
-                1st person
+                <img class="mug" src="images/team/rachel_mug.jpg" alt="Chris Harlow photo">
+                <p class="name">Rachel Donahoo</p>
+                <p class="title">Digital Marketing</p>
+                <div class="bio-button"><span>Bio</span></div>
             </div>
 
             <div class="box-3">
-                1st person
+                <img class="mug" src="images/team/jay_mug.jpg" alt="Chris Harlow photo">
+                <p class="name">Jay Crawford</p>
+                <p class="title">Graphic Design</p>
+                <div class="bio-button"><span>Bio</span></div>
             </div>
         </div>
      </section>
